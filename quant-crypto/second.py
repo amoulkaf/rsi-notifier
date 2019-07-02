acd5495ef10688171d5ee1223b3844118afc2347
Anouar amoulkaf <anouarxyz@gmail.com>
2019-07-02 21:05:15 +0200

    feat(second) : pyc

--- quant-crypto/second.py
+++ quant-crypto/second.py
@@ -9,14 +9,14 @@ import pandas as pd
 import pandas_ta as ta
 
 
-def machart(df ,kind, fast, medium, slow, append=True, last=400, figsize=(16,8)):
-   ma1 = df.ta(kind=kind, length=fast, append=append)
-   ma2 = df.ta(kind=kind, length=medium, append=append)
-   ma3 = df.ta(kind=kind, length=slow, append=append)
-   pricedf = df[['close', ma1.name, ma2.name, ma3.name]]
-   title = f"{df.name}: 1H {kind.upper()}s from {df.index[0]} to {df.index[-1]} ({last})"
-   pricedf = df[['close', ma1.name, ma2.name, ma3.name]]
-   pricedf.tail(last).plot(figsize=figsize, color=['black', 'green', 'orange', 'red'], title=title, grid=True)
+def machart(df, kind, fast, medium, slow, append=True, last=400, figsize=(16, 8)):
+    ma1 = df.ta(kind=kind, length=fast, append=append)
+    ma2 = df.ta(kind=kind, length=medium, append=append)
+    ma3 = df.ta(kind=kind, length=slow, append=append)
+    pricedf = df[['close', ma1.name, ma2.name, ma3.name]]
+    title = f"{df.name}: 1H {kind.upper()}s from {df.index[0]} to {df.index[-1]} ({last})"
+    pricedf = df[['close', ma1.name, ma2.name, ma3.name]]
+    pricedf.tail(last).plot(figsize=figsize, color=['black', 'green', 'orange', 'red'], title=title, grid=True)
 
 
 def main():
@@ -26,9 +26,9 @@ def main():
     volume = []
     highs = []
     lows = []
-    start_date = datetime.fromtimestamp(int(float(records[0][0])/1000))
-    end_date = datetime.fromtimestamp(int(float(records[len(records)-1][0])/1000))
-    index = pd.date_range(start_date,end_date,freq='1H')
+    start_date = datetime.fromtimestamp(int(float(records[0][0]) / 1000))
+    end_date = datetime.fromtimestamp(int(float(records[len(records) - 1][0]) / 1000))
+    index = pd.date_range(start_date, end_date, freq='1H')
     for record in records:
         opens.append(int(float(record[1])))
         highs.append(int(float(record[2])))
@@ -37,13 +37,13 @@ def main():
         volume.append(int(float(record[5])))
     df = pd.DataFrame({
         'index': index,
-        'open':opens,
+        'open': opens,
         'high': highs,
         'low': lows,
-        'close' : closes,
-        'volume':volume
+        'close': closes,
+        'volume': volume
     },
-    index = index)
+        index=index)
     print(index)
     df.set_index('index')
     df.name = 'BTCUSDT'
@@ -53,12 +53,5 @@ def main():
     plt.show()
 
 
-
-
-
-   
-        
-
 if __name__ == '__main__':
     main()
-
