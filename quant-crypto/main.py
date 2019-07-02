a835dcaef5919729204e8c2d5bfe1873ec54d3ae
amoulkaf <anouar.moulkaf@gmail.com>
2019-07-02 14:08:22 +0200

    feat(divergence) : peaks

--- quant-crypto/main.py
+++ quant-crypto/main.py
@@ -1,14 +1,29 @@
 import json
-import pandas as pd
-import pandas_ta as ta
-import requests
 import time
-import numpy as np
-from sklearn.linear_model import LinearRegression
+from scipy.signal import find_peaks
+from scipy.misc import electrocardiogram
+import requests
 from quant import linear_regression
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+import numpy as np
 
 _DAY_ = 86400 * 1000
 
+def newline(p1, p2):
+    ax = plt.gca()
+    xmin, xmax = ax.get_xbound()
+
+    if(p2[0] == p1[0]):
+        xmin = xmax = p1[0]
+        ymin, ymax = ax.get_ybound()
+    else:
+        ymax = p1[1]+(p2[1]-p1[1])/(p2[0]-p1[0])*(xmax-p1[0])
+        ymin = p1[1]+(p2[1]-p1[1])/(p2[0]-p1[0])*(xmin-p1[0])
+
+    l = mlines.Line2D([xmin,xmax], [ymin,ymax], color="Red")
+    ax.add_line(l)
+    return l
 
 def main():
     now = int(time.time()) * 1000
@@ -23,7 +38,21 @@ def main():
     records = json.loads(r.text)
 
     for record in records:
-        closes.append(float(record[4]))
+        closes.append(int(float(record[4])))
+
+    print("closes : %s" % closes)
+    # x = electrocardiogram(closes)
+    peaks, _ = find_peaks(closes, height=0)
+    plt.plot(closes)
+
+    # plt.plot(peaks, closes[peaks], "x")
+    plt.plot(np.zeros_like(closes), "--", color="gray")
+    p1 = [peaks[0], closes[peaks[0]]]
+    p2 = [peaks[1], closes[peaks[1]]]
+    newline(p1, p2)
+    plt.show()
+
+    print("peaks : %s" % peaks)
 
     rsi_lr = linear_regression.rsi_linear_regression(closes)
     print(rsi_lr)
