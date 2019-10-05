99e05a408afff078005d564adf2a81ae3c312b56
bmoulkaf <moulkaf.badreddine@gmail.com>
2019-10-05 23:08:49 +0000

    feat(bearish rsi divergence)

--- quant-crypto/signals/rsi_divergence.py
+++ quant-crypto/signals/rsi_divergence.py
@@ -24,3 +24,16 @@ class RsiDivergence:
         ldf["Lpeaksdif"] = ldf["Lpeaks"] - ldf["Lpeaks"].shift(1)
         ldf["Lrsidiv"] = ((ldf["rsidif"] > 0) & (ldf["Lpeaksdif"] < 0))
         return ldf["Lrsidiv"]
+    def bearishRsi(self):
+        df = self.df
+        closes = df['closes'].as_matrix()
+        hpeaks = scipy.signal.argrelmax(closes, order=self.order)
+        df['Hpeaks'] = None
+        for i in hpeaks[0]:
+            df['Hpeaks'].iloc[i] = df['closes'].iloc[i]
+        df['rsi']=ta.rsi(df['closes'])
+        hdf= df[(df['Hpeaks'].notnull())]
+        hdf["rsidif"] = hdf["rsi"] - hdf["rsi"].shift(1)
+        hdf["Hpeaksdif"] = hdf["Hpeaks"] - hdf["Hpeaks"].shift(1)
+        hdf["Hrsidiv"] = ((hdf["rsidif"] < 0) & (hdf["Hpeaksdif"] > 0))
+        return hdf["Hrsidiv"]
